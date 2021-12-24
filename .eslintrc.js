e5c17c3568e9312a965b2fdc59a638f92d96882c
Paul Su <paul.su@ohbottech.com>
2021-12-24 12:53:06 +0800

    refinement install eslint config command

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'bytest/lib/vue3-essential-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  globals: {
+  },
+  rules: {
+
+  }
+}
